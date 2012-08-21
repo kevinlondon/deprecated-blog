7eac8615658534f870a67f9e85b6af877044673c
Kaedon <kevinlondon@gmail.com>
2012-08-20 21:05:58 -0700

    Add tags, rework urls to use generic view. Following http://tinyurl.com/7freh7b screencast.

--- blog/models.py
+++ blog/models.py
@@ -1,17 +1,11 @@
 from django.db import models
+from taggit.managers import TaggableManager
 
 class BlogPost(models.Model):
-    title = models.CharField(max_length=60)
+    title = models.CharField(max_length=100)
     body = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
+    tags = TaggableManager()
 
     def __unicode__(self):
         return self.title
-
-    """
-    @models.permalink
-    def get_absolute_url(self):
-        url_name = "blog_post_detail"
-        kwargs = {"slug": self.slug}
-        return (url_name, (), kwargs)
-    """        
--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls.defaults import patterns, url
-
+from django.views.generic import ListView
+from blog.models import BlogPost
 # Blog patterns
 urlpatterns = patterns('blog.views',
-    url(r'^$', 'blog_post_list'),
+    url(r'^$', ListView.as_view(
+        queryset=BlogPost.objects.all().order_by("-created")[:2],
+        template_name="blog/list.html")),
 )
